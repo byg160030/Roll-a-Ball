0b85a92407004a47d2b834bf3ede7fb114e4b2cd
Brice Yuuki Gibson <byg160030@utdallas.edu>
2022-05-16 17:53:21 -0500

    Game Completed
    Player can now know how much collectables they have collected and a win screen is added to the game.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,21 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
 
     private Rigidbody rb;
-
+    private int count;
     private float movementX;
     private float movementY;
 
     // Start is called before the first frame update
     private void Start()
     {
+        // Assign the Rigidbody component to our private rb variable
         rb = GetComponent<Rigidbody>();
+
+        // Set the count to zero
+        count = 0;
+
+        SetCountText();
+
+        // Set the text property of the Win Text UI to an empty string, making the 'You Win' (game over message) blank
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -25,6 +37,16 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            // Set the text value of your 'winText'
+            winTextObject.SetActive(true);
+        }
     }
     
     void FixedUpdate()
@@ -36,9 +58,16 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        // ..and if the GameObject you intersect has the tag 'Pick Up' assigned to it..
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+
+            // Add one to the score variable 'count'
+            count = count + 1;
+
+            // Run the 'SetCountText()' function (see below)
+            SetCountText();
         }
     }
 }
